18d58c1d1a8bd032079bb0a4da2e98e4fb17bc15
Taylor Bisset <taylorbisset@gmail.com>
2023-10-19 10:36:58 -0700

    Copy over TBString.h from v1

---
+++ CS132-Program3-MYString-v2/TBString.h
@@ -0,0 +1,72 @@
+#ifndef TBSTRING_H
+// TBString.h
+#define TBSTRING_H
+
+#include <istream>
+
+using namespace std;
+
+class TBString
+{    // You NEED to change the name of the class to reflect your name 
+                    //   for example Nancy Programmer would use the name NPString 
+public:
+    TBString();						// default constructor
+    TBString(const char* cstr);		// cstring constructor
+
+    int length();
+    int capacity();
+
+    char at(int index);
+    bool read(istream& inputStrm);
+    void write(ostream& outputStrm);
+
+    bool lessThan(const TBString& argStr);
+    bool greaterThan(const TBString& argStr);
+    bool equals(const TBString& argStr);
+
+    void setEqualTo(const TBString& argStr);
+    const char* c_str();
+
+private:
+    int length(const char* str)
+    {
+        int len;
+        for (len = 0; str[len] != '\0'; len++) {}
+        return len;
+    }
+
+    int compareTo(const char* lstr, const char* rstr)
+    {
+        while (*lstr != '\0' && *rstr != '\0')
+        {
+            if (*lstr < *rstr)
+            {
+                return -1;
+            }
+            else if (*lstr > *rstr)
+            {
+                return 1;
+            }
+            lstr++;
+            rstr++;
+        }
+        if (length(lstr) < length(rstr))
+        {
+            return -1;
+        }
+        else if (length(lstr) > length(rstr))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    char* str;
+    int end;
+    int cap;
+};
+
+#endif
